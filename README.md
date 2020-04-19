c11a334865519112859542cf6ec4df5fa1167a5b
amirmasoud <amir.masoud.sefidian@gmail.com>
2020-04-20 01:25:57 +0430

    Initialze the project with the materials

---
+++ README.md
@@ -0,0 +1,31 @@
+# Disaster Response Pipeline Project
+
+### Project Description:
+
+In this project, I built ETL and machine learning pipelines that is capable to curate the class of the messages. The pipeline is eventually built into a flask application. The project include a web app where an emergency worker can input a new message and get classification results in several categories (Multi-Label Classification). The landing page of the webapp also includes four visualizations of the training dataset built with plotly.
+
+### File Descriptions:
+The project contains the following files,
+
+* ETL Pipeline Preparation.ipynb: Notebook experiment for the ETL pipelines
+* ML Pipeline Preparation.ipynb: Notebook experiment for the machine learning pipelines
+* data/process_data.py: The ETL pipeline used to process data in preparation for model building.
+* models/train_classifier.py: The Machine Learning pipeline used to fit, tune, evaluate, and export the model to a Python pickle (pickle is not uploaded to the repo due to size constraints on github).
+* app: flask application folder. run.py starts the python server for the web app and prepare visualizations.
+
+
+## Usage:
+
+1. Run the following commands in the project's root directory to set up your database and model.
+
+* To run ETL pipeline that cleans data and stores in sql database:
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+* To run ML pipeline that trains classifier and saves the model:
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier_me.pkl`
+
+2. Run the following command in the app's directory to run your web app:
+    `python run.py`
+
+3. Go to http://127.0.0.1:3001/ to check out the API.
+
+![Webapp Screenshot](https://raw.githubusercontent.com/amirmasoudsfd/disaster_response_app/master/disasterapp_screenshot.pnj)
---
+++ app/disasterapp/routes.py
@@ -0,0 +1,180 @@
+import json
+import plotly
+import pandas as pd
+import re
+from collections import Counter
+
+from disasterapp import app
+import json, plotly
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+from disasterapp.tokenizer_function import tokenize_only_english
+from util_scripts.tokenizer_function import tokenize, Tokenizer
+from util_scripts import tokenizer_function
+
+
+@app.before_first_request
+def load_model_data():
+    global df
+    global model
+    # load data
+
+    engine = create_engine('sqlite:///../data/DisasterResponse.db')
+    df = pd.read_sql_table('DisasterResponse', engine)
+    model = joblib.load("../models/classifier_me.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    # extract data needed for visuals
+    # Message counts of different generes
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    genre_counts = genre_counts.to_list()
+
+    # Message counts for different categories
+    cate_counts_df = df.iloc[:, 4:].sum().sort_values(ascending=False)
+    cate_counts = list(cate_counts_df)
+    cate_names = list(cate_counts_df.index)
+
+    # Top keywords in Social Media in percentages
+    social_media_messages = ' '.join(df[df['genre'] == 'social']['message'])
+    social_media_tokens = tokenize_only_english(social_media_messages)
+
+    # social_media_tokens = tokenize(social_media_messages)
+    social_media_wrd_counter = Counter(social_media_tokens).most_common()
+
+    items, counts = zip(*social_media_wrd_counter)
+    word_frequency = pd.Series(counts, index=items) / sum(counts) * 100
+    social_media_wrds = word_frequency.index[:50].to_list()
+    social_media_wrd_pct = word_frequency[:50].to_list()
+
+
+    # direct_messages = ' '.join(df[df['genre'] == 'direct']['message'])
+    #
+    # direct_tokens = tokenize_only_english(direct_messages)
+    # # social_media_tokens = tokenize(social_media_messages)
+    # direct_wrd_counter = Counter(direct_tokens).most_common()
+    #
+    # items, counts = zip(*direct_wrd_counter)
+    # word_frequency = pd.Series(counts, index=items) / sum(counts) * 100
+    # direct_wrds = word_frequency.index[:50].to_list()
+    # direct_wrd_pct = word_frequency[:50].to_list()
+
+    # Top keywords in Direct in percentages
+    direct_messages = ' '.join(df[df['genre'] == 'direct']['message'])
+    direct_tokens = tokenize(direct_messages)
+    direct_wrd_counter = Counter(direct_tokens).most_common()
+    direct_wrd_cnt = [i[1] for i in direct_wrd_counter]
+    direct_wrd_pct = [i / sum(direct_wrd_cnt) * 100 for i in direct_wrd_cnt]
+    direct_wrds = [i[0] for i in direct_wrd_counter]
+    # create visuals
+
+    graphs = [
+        # Histogram of the message genere
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        # histogram of social media messages top 30 keywords
+        {
+            'data': [
+                Bar(
+                    x=social_media_wrds[:50],
+                    y=social_media_wrd_pct[:50]
+                )
+            ],
+
+            'layout': {
+                'title': "Top 50 Keywords in Social Media Messages",
+                'xaxis': {'tickangle': 60
+                          },
+                'yaxis': {
+                    'title': "% Total Social Media Messages"
+                }
+            }
+        },
+
+        # histogram of direct messages top 30 keywords
+        {
+            'data': [
+                Bar(
+                    x=direct_wrds[:50],
+                    y=direct_wrd_pct[:50]
+                )
+            ],
+
+            'layout': {
+                'title': "Top 50 Keywords in Direct Messages",
+                'xaxis': {'tickangle': 60
+                          },
+                'yaxis': {
+                    'title': "% Total Direct Messages"
+                }
+            }
+        },
+
+        # histogram of messages categories distributions
+        {
+            'data': [
+                Bar(
+                    x=cate_names,
+                    y=cate_counts
+                )
+            ],
+
+            'layout': {
+                'title': "Distribution of Message Categories",
+                'xaxis': {'tickangle': 60
+                          },
+                'yaxis': {
+                    'title': "count"
+                }
+            }
+        },
+
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
